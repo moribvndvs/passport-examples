c79b4fb52eb96368d480aef8a5f59d1e1760cd5a
Mike Grabski <me@mikegrabski.com>
2018-03-30 19:12:06 -0400

    fix social media login problem when client is built /auth/spotify and /auth/twitter never finalize the request, causing express.static and the route.get(*) routes to try and serve the index.html. adding a controller function that simply does res.end() fixes that

--- example-social-media-react/routes/htmlRoutes.js
+++ example-social-media-react/routes/htmlRoutes.js
@@ -14,7 +14,9 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
 
 // we need to define the routes necessary to make passport-spotify work
 // here's the URL users need to visit to initiate the spotify login
-router.get('/auth/spotify', passport.authenticate('spotify'));
+router.get('/auth/spotify', passport.authenticate('spotify'), (req, res) => {
+  res.end();
+});
 // here's the URL spotify will call back to finish logging them into your site
 router.get('/auth/spotify/callback', passport.authenticate('spotify', {
   failureRedirect: `${baseUrl}/auth/failed`, // tell it where to go if they couldn't log in
@@ -23,7 +25,11 @@ router.get('/auth/spotify/callback', passport.authenticate('spotify', {
 
 // we need to define the routes necessary to make passport-twitter work
 // here's the URL users need to visit to initiate the twitter login
-router.get('/auth/twitter', passport.authenticate('twitter'));
+// the res.end() function is a hack to fix a problem where the express static
+// middleware tries to serve the files out of client/build
+router.get('/auth/twitter', passport.authenticate('twitter'), (req, res) => {
+  res.end();
+});
 // here's the URL twitter will call back to finish logging them into your site
 router.get('/auth/twitter/callback', passport.authenticate('twitter', {
   failureRedirect: `${baseUrl}/auth/failed`, // tell it where to go if they couldn't log in
@@ -31,7 +37,9 @@ router.get('/auth/twitter/callback', passport.authenticate('twitter', {
 }));
 
 // Serve up static assets (usually on heroku)
-router.use(express.static("client/build"));
+router.use(express.static("client/build", {
+  index: false
+}));
 
 // Send every request to the React app
 // Define any API or static HTML routes before this runs!
